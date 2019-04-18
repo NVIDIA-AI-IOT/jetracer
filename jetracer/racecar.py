a7131fb6f7a4e652f6e641b0a6b63b263f8abf09
John Welsh <jwelsh@nvidia.com>
2019-04-18 16:06:44 -0700

    added image to racecar

--- jetracer/racecar.py
+++ jetracer/racecar.py
@@ -3,5 +3,6 @@ import traitlets
 
 
 class Racecar(traitlets.HasTraits):
+    image = traitlets.Any()
     steering = traitlets.Float(min=-1.0, max=1.0)
     throttle = traitlets.Float(min=-1.0, max=1.0)
