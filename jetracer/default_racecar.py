0f6340a1d653537640213a8d2f25b073f67c7227
John Welsh <jwelsh@nvidia.com>
2019-04-18 15:45:59 -0700

    racecar

--- jetracer/default_racecar.py
+++ jetracer/default_racecar.py
@@ -8,4 +8,11 @@ class DefaultRacecar(Racecar):
     def __init__(self, *args, **kwargs):
         kit = ServoKit(channels=16)
         self.steering_motor = AdafruitServoKitMotor(kit=kit, channel=0)
-        self.throttle_motor = AdafruitServoKitMotor(kit=kit, channel=1, alpha=-1.0)
+        self.throttle_motor = AdafruitServoKitMotor(kit=kit, channel=1)
+        
+        # link throttle
+        traitlets.dlink((self, 'throttle'), (self.throttle_motor, 'value'), transform=lambda x: -x)
+        traitlets.dlink((self.throttle_motor, 'value'), (self, 'throttle'), transform=lambda x: -x)
+        
+        # link steering
+        traitlets.link((self, 'steering'), (self.steering_motor, 'value'))
--- jetracer/racecar.py
+++ jetracer/racecar.py
@@ -3,5 +3,5 @@ import traitlets
 
 
 class Racecar(traitlets.HasTraits):
-    steering_motor = traitlets.Instance(klass=Motor)
-    throttle_motor = traitlets.Instance(klass=Motor)
+    steering = traitlets.Float(min=-1.0, max=1.0)
+    throttle = traitlets.Float(min=-1.0, max=1.0)
