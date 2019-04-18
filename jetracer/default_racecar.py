ecf12fdfe72af4e7fb80b31b952c5300d91ba1c4
John Welsh <jwelsh@nvidia.com>
2019-04-18 15:23:22 -0700

    refactor

--- jetracer/default_racecar.py
+++ jetracer/default_racecar.py
@@ -1,11 +1,11 @@
-from racecar import Racecar
+from .racecar import Racecar
 import traitlets
 from adafruit_servokit import ServoKit
 from jetmotor.adafruit_servokit_motor import AdafruitServoKitMotor
 
 
-class NvidiaRacecar(Racecar):
+class DefaultRacecar(Racecar):
     def __init__(self, *args, **kwargs):
-        kit = ServoKit()
+        kit = ServoKit(channels=16)
         self.steering_motor = AdafruitServoKitMotor(kit=kit, channel=0)
-        self.throttle_motor = AdafruitServoKitMotor(kit=kit, channel=1)
+        self.throttle_motor = AdafruitServoKitMotor(kit=kit, channel=1, alpha=-1.0)
