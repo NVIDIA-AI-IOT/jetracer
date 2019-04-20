5e499fc04bd87dde6322e12c770026d4abf106b9
John Welsh <jwelsh@nvidia.com>
2019-04-19 19:16:16 -0700

    update

--- jetracer/default_racecar.py
+++ jetracer/default_racecar.py
@@ -8,12 +8,8 @@ from jetcam.imx219 import IMX219
 class DefaultRacecar(Racecar):
     def __init__(self, *args, **kwargs):
         kit = ServoKit(channels=16)
-        self.camera = IMX219(capture_width=1280, capture_height=720, fps=30, width=224, height=224)
         self.steering_motor = AdafruitServoKitMotor(kit=kit, channel=0)
         self.throttle_motor = AdafruitServoKitMotor(kit=kit, channel=1)
-        
-        # link camera
-        traitlets.link((self.camera, 'value'), (self, 'image'))
         
         # link throttle
         traitlets.dlink((self, 'throttle'), (self.throttle_motor, 'value'), transform=lambda x: -x)
--- jetracer/racecar.py
+++ jetracer/racecar.py
@@ -3,6 +3,5 @@ import traitlets
 
 
 class Racecar(traitlets.HasTraits):
-    image = traitlets.Any()
     steering = traitlets.Float(min=-1.0, max=1.0)
     throttle = traitlets.Float(min=-1.0, max=1.0)
