5504666c68ef37e39c229b6d9ba8bce9cd48e655
John Welsh <jwelsh@nvidia.com>
2019-08-13 15:44:48 -0700

    data collection nb

--- notebooks/apex_tracking/xy_dataset.py
+++ notebooks/apex_tracking/xy_dataset.py
@@ -10,6 +10,7 @@ import numpy as np
 
 
 class XYDataset(torch.utils.data.Dataset):
+    
     def __init__(self, directory, transform=None, random_hflip=False):
         super(XYDataset, self).__init__()
         self.directory = directory
@@ -18,15 +19,20 @@ class XYDataset(torch.utils.data.Dataset):
         self.random_hflip = random_hflip
         
     def __len__(self):
-        return len(self.annotations)
+        return len(self.image_paths)
     
     def __getitem__(self, idx):
         
-        ann = self.annotations[idx]
+        image_path = self.image_paths[idx]
+        
+        x = int(image_path.split('_')[0])
+        y = int(image_path.split('_')[1])
+        
         image = cv2.imread(ann['image_path'], cv2.IMREAD_COLOR)
         image = PIL.Image.fromarray(image)
         width = image.width
         height = image.height
+        
         if self.transform is not None:
             image = self.transform(image)
         
@@ -37,44 +43,19 @@ class XYDataset(torch.utils.data.Dataset):
             image = torch.from_numpy(image.numpy()[..., ::-1].copy())
             x = -x
             
-        return image, ann['category_index'], torch.Tensor([x, y])
+        return image, torch.Tensor([x, y])
     
-    def _parse(self, path):
-        basename = os.path.basename(path)
-        items = basename.split('_')
-        x = items[0]
-        y = items[1]
-        return int(x), int(y)
-        
     def refresh(self):
-        
-        self.annotations = []
-        for category in self.categories:
-            category_index = self.categories.index(category)
-            for image_path in glob.glob(os.path.join(self.directory, category, '*.jpg')):
-                x, y = self._parse(image_path)
-                self.annotations += [{
-                    'image_path': image_path,
-                    'x': x,
-                    'y': y
-                }]
+        self.image_paths = glob.glob(os.path.join(self.directory, '*.jpg'))
         
     def save_entry(self, image, x, y):
-        image_filename = '%d_%d_' + str(uuid.uuid1()) + '.jpg'
-        with open()
-        category_dir = os.path.join(self.directory, category)
-        if not os.path.exists(category_dir):
-            subprocess.call(['mkdir', '-p', category_dir])
-            
         filename = '%d_%d_%s.jpg' % (x, y, str(uuid.uuid1()))
+        image_path = os.path.join(self.directory, filename)
         
-        image_path = os.path.join(category_dir, filename)
-        cv2.imwrite(image_path, image)
+        if not os.path.exists(self.directory):
+            subprocess.call(['mkdir', '-p', self.directory])
+            
+        with open(image_path, 'wb') as f:
+            cv2.imwrite(image_path, image)
+            
         self.refresh()
-        
-    def get_count(self, category):
-        i = 0
-        for a in self.annotations:
-            if a['category'] == category:
-                i += 1
-        return i
