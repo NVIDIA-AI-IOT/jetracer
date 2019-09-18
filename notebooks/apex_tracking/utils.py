495da40cb0c6f15d6608c43f25b3438f02269985
John Welsh <jwelsh@nvidia.com>
2019-09-18 13:25:31 -0700

    update half

--- notebooks/apex_tracking/utils.py
+++ notebooks/apex_tracking/utils.py
@@ -5,12 +5,12 @@ import cv2
 import PIL.Image
 import numpy as np
 
-mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
-std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
+mean = torch.Tensor([0.485, 0.456, 0.406]).cuda().half()
+std = torch.Tensor([0.229, 0.224, 0.225]).cuda().half()
 
 def preprocess(image):
     device = torch.device('cuda')
     image = PIL.Image.fromarray(image)
-    image = transforms.functional.to_tensor(image).to(device)
+    image = transforms.functional.to_tensor(image).half().to(device)
     image.sub_(mean[:, None, None]).div_(std[:, None, None])
     return image[None, ...]
